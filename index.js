7f042096b5755b059d6c431a30cd3cf0b6238384
Venal <s22310263@student.unklab.ac.id>
2025-11-05 07:00:25 +0800

    install react

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import SplashScreen from './src/pages/SplashScreen';
 import SignIn from './src/pages/SignIn';
+import SignIn from './src/pages/SignIn';
 
 AppRegistry.registerComponent(appName, () => SignIn);
